6309778bbe9d2ed7e57e29a21d3b42d1bf4a8980
sanvi-ul-kayes <kayesmolla124@gmail.com>
2025-07-04 11:15:07 +0600

    class-13 complete fully

--- Router/api/cart.js
+++ Router/api/cart.js
@@ -1,8 +1,14 @@
 const express = require("express");
-const createCartController = require("../../controllers/cartController");
+const {
+  createCartController,
+  singleUserCartController,
+} = require("../../controllers/cartController");
 const router = express.Router();
 
 //localhost:7878/api/v1/cart/createCart
 router.post("/createCart", createCartController);
+
+//localhost:7878/api/v1/cart/singleUserCart
+router.get("/singleUserCart/:userId", singleUserCartController);
 
 module.exports = router;
--- controllers/cartController.js
+++ controllers/cartController.js
@@ -1,6 +1,37 @@
+const cartSchema = require("../dbSchema/cartSchema");
+
 //localhost:7878/api/v1/cart/createCart
 async function createCartController(req, res) {
-  res.send("createCart");
+  let { price, quantity, product, user } = req.body;
+  try {
+    if (!price || !quantity || !product || !user) {
+      res.status(400).json({ error: "Missing required fields" });
+    } else {
+      const createCart = new cartSchema({ price, quantity, product, user });
+      await createCart.save();
+      res.status(201).send({
+        success: true,
+        msg: "Product add to Cart successfully",
+        data: createCart,
+      });
+    }
+  } catch (error) {
+    res.status(404).send({ data: error });
+  }
 }
 
-module.exports = createCartController;
+//localhost:7878/api/v1/cart/singleUserCart
+async function singleUserCartController(req, res) {
+  let { userId } = req.params;
+  try {
+    const singleUserCart = await cartSchema.findOne({ user: userId });
+    res.status(200).send({
+      success: true,
+      msg: "Single User Cart is successfully fetched",
+      data: singleUserCart,
+    });
+  } catch (error) {
+    res.status(404).send({ data: error ? error : "Can't fetch" });
+  }
+}
+module.exports = { createCartController, singleUserCartController };
